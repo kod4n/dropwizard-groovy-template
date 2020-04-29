0c540760580bac91ab686f1f39b39fdfeffd1f6d
Andrew Allen <anallen@cisco.com>
2020-04-29 11:40:52 -0400

    testing tagged builds
    Signed-off-by: Andrew Allen <anallen@cisco.com>

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -7,11 +7,16 @@ set -o errexit
 TRAVIS_BUILDARGS=(--build-arg "TRAVIS=${TRAVIS}" --build-arg "TRAVIS_JOB_ID=${TRAVIS_JOB_ID}")
 
 echo "[travis_docker_build] running base docker build"
-docker build "${TRAVIS_BUILDARGS[@]}" --target build .
+echo "docker build ${TRAVIS_BUILDARGS[*]} --target build ."
+#docker build "${TRAVIS_BUILDARGS[@]}" --target build .
 
 docker_tag="${TRAVIS_TAG:-latest}"
 echo "[travis_docker_build] packaging docker images for tag [${docker_tag}]"
-docker build "${TRAVIS_BUILDARGS[@]}" \
-             --tag "${TRAVIS_REPO_SLUG}:${docker_tag}" \
-             --tag "quay.io/${TRAVIS_REPO_SLUG}:${docker_tag}" \
-             --target package .
+echo "docker build ${TRAVIS_BUILDARGS[*]}
+             --tag ${TRAVIS_REPO_SLUG}:${docker_tag}
+             --tag quay.io/${TRAVIS_REPO_SLUG}:${docker_tag}
+             --target package ."
+#docker build "${TRAVIS_BUILDARGS[@]}" \
+#             --tag "${TRAVIS_REPO_SLUG}:${docker_tag}" \
+#             --tag "quay.io/${TRAVIS_REPO_SLUG}:${docker_tag}" \
+#             --target package .
